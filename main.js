393679f2582e3f60f7c80323c6095baca1fe6521
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-01-20 15:19:22 -0500

    fusión del menú mobile

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const $navbar_email = document.querySelector('.navbar-email')
+const $menu_ham = document.querySelector('.menu')
 const $desktop_menu = document.querySelector('.desktop-menu')
+const $mobile_menu = document.querySelector('.mobile-menu')
 
 $navbar_email.addEventListener('click', () => {
     $desktop_menu.classList.toggle('inactive')
     console.log('presionado papu')
+})
+
+$menu_ham.addEventListener('click', () => {
+    $mobile_menu.classList.toggle('inactive')
+    console.log('presionado mobile')
 })
